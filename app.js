15b187225d0d115b4b013f13a07fd06fa70ca526
unknown <mail.ahliwasum@gmail.com>
2022-03-10 15:26:40 +0530

    variables

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ console.log("Hello from Typescript");
 function add(n1, n2) {
     return n1 + n2;
 }
-var number1 = '5';
+var number1 = 5; //'5'
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
+/////////////TYPES////////
+var number5 = 5;
+var numberUnknown;
+var variableAny;
+var isTrue;
+var stringVal = ' Hello How are you?';
+// ANY type can be used to store any value
+variableAny = stringVal;
+console.log('variableAny' + variableAny);
+variableAny = number5;
+console.log('variableAny ' + variableAny);
--- app.ts
+++ app.ts
@@ -7,8 +7,24 @@ function add(n1:number,n2:number)
 }
 
 
-const number1= '5'
+const number1= 5//'5'
 const number2 = 2.8
 
 const result= add(number1,number2);
 console.log(result)
+
+
+/////////////TYPES////////
+let number5 = 5;
+let numberUnknown: number;
+let variableAny:any;
+let isTrue:boolean;
+let stringVal=' Hello How are you?';
+
+// ANY type can be used to store any value
+
+variableAny=stringVal;
+console.log('variableAny'+variableAny);
+
+variableAny=number5;
+console.log('variableAny '+variableAny);
